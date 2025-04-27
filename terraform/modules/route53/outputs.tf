ce6d6c76428ea3b8e6b02950309e2ee0386b126d
HamzahS-101 <hamzahsahal@gmail.com>
2025-04-27 20:23:33 +0100

    Add hosted zone id output

--- terraform/modules/route53/outputs.tf
+++ terraform/modules/route53/outputs.tf
@@ -2,3 +2,8 @@ output "zone_arn" {
   description = "Hosted Zone arn"
   value       = aws_route53_zone.zone.arn
 }
+
+output "zone_id" {
+  description = "Hosted Zone ID"
+  value       = aws_route53_zone.zone.id
+}
